73b02063bc216234dd5a2f07f9455bf4f7312486
Aryan Mishra <arycodes@gmail.com>
2024-06-01 23:01:09 +0530

    update

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -19,9 +19,9 @@ const Navbar = ({ isOpen }) => {
 
       <div className="nav-footer">
         {/* <a href="https://github.com/arycodes/arybot-2?tab=MIT-1-ov-file" id='terms'>Terms and Condition</a> */}
-        {/* <span>made by <a href="https://aryanm.web.app">me</a> with
+        <span>Made by       <a href="https://aryn.in" target="_blank" rel="noopener noreferrer">ME</a>          with
           <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" ><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z" ></path> </g></svg>
-        </span> */}
+        </span>
 
       </div>
 
